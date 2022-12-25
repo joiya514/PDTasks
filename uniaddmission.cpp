a4e8bd5285a0487ea707a7b8d7455289006941f0
joiya514 <121043606+joiya514@users.noreply.github.com>
2022-12-25 19:03:19 +0500

    Add files via upload

---
+++ uniaddmission.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main(){
+cout << "***********************************"<< endl;
+cout << "*                                 *"<< endl;
+cout << "*      University Addmission      *"<< endl;
+cout << "*        Management System        *"<< endl;
+cout << "*                                 *"<< endl;
+cout << "*                                 *"<< endl;
+cout << "***********************************"<< endl;
+
+
+
+
+
+}
